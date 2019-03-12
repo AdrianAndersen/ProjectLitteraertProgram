c5b6aec93698426b31ffeaffa6d38097d6d4b002
Adrian Arthur Andersen <adrian.arthur.andersen@gmail.com>
2019-03-12 14:11:20 +0100

    endret div filnavn

--- scripts/spillDialekt.js
+++ scripts/spillDialekt.js
@@ -1,1 +1,10 @@
-// JavaScript source code
+(() => {
+    "use strict";
+    function startSpill(e) {
+        e.preventDefault();
+        // Fokus på input
+        console.log("starter spill");
+    }
+    document.querySelector("#form").addEventListener("submit", startSpill);
+    document.querySelector("#knappStartSpill").addEventListener("click", startSpill);
+})();
