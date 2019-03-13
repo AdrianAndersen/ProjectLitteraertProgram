75da0f5cc9552930355618998198a531bbd6e4be
emlie <mi-ya@live.no>
2019-03-13 11:09:04 +0100

    add base style to dialektinfo

--- scripts/infoDialekt.js
+++ scripts/infoDialekt.js
@@ -3,6 +3,11 @@
 
 /* ---------- DOM ---------- */
 
+let nav = document.querySelector('nav');
+
+let main = document.querySelector('main');
+let infoDialekt = document.querySelector('#infoDialekt');
+
 let footer = document.querySelector('footer');
 
 
@@ -11,8 +16,70 @@ let footer = document.querySelector('footer');
 
 /* ---------- GET CSS VALUES ---------- */
 
+let navCompHeight = window.getComputedStyle(nav).height;
+let navHeight = pxToNum(navCompHeight);
+
 let footerCompHeight = window.getComputedStyle(footer).height;
 let footerHeight = pxToNum(footerCompHeight);
+
+
+
+
+
+/* ---------- DATA ---------- */
+
+let data = [
+  {
+    type: 'palatalisering',
+    desc: 'en eller annen slags forklaring på dette målmerket',
+    geo: 'nord, sør, øst, vest'
+  },
+  {
+    type: 'tjukk l',
+    desc: 'en eller annen slags forklaring på dette målmerket',
+    geo: 'nord, sør, øst, vest'
+  },
+  {
+    type: 'skarre-r',
+    desc: 'en eller annen slags forklaring på dette målmerket',
+    geo: 'nord, sør, øst, vest'
+  },
+  {
+    type: 'apokope',
+    desc: 'en eller annen slags forklaring på dette målmerket',
+    geo: 'nord, sør, øst, vest'
+  },
+  {
+    type: 'bløte konsonanter',
+    desc: 'en eller annen slags forklaring på dette målmerket',
+    geo: 'nord, sør, øst, vest'
+  },
+  {
+    type: 'nektingsadverbet "ikke"',
+    desc: 'en eller annen slags forklaring på dette målmerket',
+    geo: 'nord, sør, øst, vest'
+  },
+  {
+    type: 'personlig pronomen',
+    desc: 'en eller annen slags forklaring på dette målmerket',
+    geo: 'nord, sør, øst, vest'
+  },
+  {
+    type: 'målmerke 8',
+    desc: 'en eller annen slags forklaring på dette målmerket',
+    geo: 'nord, sør, øst, vest'
+  },
+  {
+    type: 'målmerke 9',
+    desc: 'en eller annen slags forklaring på dette målmerket',
+    geo: 'nord, sør, øst, vest'
+  },
+  {
+    type: 'målmerke 10',
+    desc: 'en eller annen slags forklaring på dette målmerket',
+    geo: 'nord, sør, øst, vest'
+  },
+];
 
 
 
@@ -29,13 +96,62 @@ function pxToNum(string) {
 
 
 
+/* ---------- SET MAIN MARGIN TOP ---------- */
+
+function mainTop() {
+  // adjust margin top
+  main.style.setProperty('--mainTop', `-${5*navHeight}px`);
+}
+
+
+
+
+
+/* ---------- SET ROW NUM ---------- */
+
+function rowNum() {
+  // adjust columns in info text
+  infoDialekt.style.setProperty('--rowDialekt', `${data.length/2}`);
+}
+
+
+
+
+
 /* ---------- SET FOOTER MARGIN TOP ---------- */
 
 function footerTop() {
   // hide footer from start view
-  footer.style.setProperty('--footerTop', `-${2*footerHeight}px`);
+  footer.style.setProperty('--footerTop', `-${4*footerHeight}px`);
+}
 
-  console.log('fix footer');
+
+
+
+
+/* ---------- DISPLAY DATA ---------- */
+
+function insertData() {
+
+  data.forEach(type => {
+    infoDialekt.innerHTML += `
+      <div class="info">
+        <h2>${type.type}</h2>
+        <div class="text">
+          <p>
+            ${type.desc}
+          </p>
+          <p>
+            områder: <br>
+            ${type.geo}
+          </p>
+        </div>
+      </div>
+    `;
+  });
+
+  console.log('insertData');
+
 }
 
 
@@ -44,4 +160,8 @@ function footerTop() {
 
 /* ---------- REGISTER FUNCTIONS ---------- */
 
+mainTop();
+rowNum();
 footerTop();
+
+insertData();
