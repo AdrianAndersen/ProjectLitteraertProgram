84c174a69e804195b2ee81e6f60e87817e142ed6
emlie <mi-ya@live.no>
2019-03-16 14:51:50 +0100

    style spillDialekt

--- scripts/spillDialekt.js
+++ scripts/spillDialekt.js
@@ -237,3 +237,41 @@ function finnAvstand(p1, p2) {
 };
 
 
+
+
+
+
+
+
+
+
+/* ---------- SET MARGIN FOR MAIN AND FOOTER ---------- */
+
+function pxToNum(string) {
+  // remove 'px' at the end and turn string to number
+  return Number(string.slice(0, string.length - 2));
+}
+
+function mainTop() {
+  let nav = document.querySelector('nav');
+  let main = document.querySelector('main');
+
+  let navCompHeight = window.getComputedStyle(nav).height;
+  let navHeight = pxToNum(navCompHeight);
+
+  // adjust margin top
+  main.style.setProperty('--mainTop', `${navHeight}px`);
+}
+
+function footerTop() {
+  let footer = document.querySelector('footer');
+
+  let footerCompHeight = window.getComputedStyle(footer).height;
+  let footerHeight = pxToNum(footerCompHeight);
+
+  // hide footer from start view
+  footer.style.setProperty('--footerTop', `-${3*footerHeight}px`);
+}
+
+mainTop();
+footerTop();
