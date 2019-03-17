9bff9b396c1863bf60e13fa9443dc4fc53e9950c
Adrian Arthur Andersen <adrian.arthur.andersen@gmail.com>
2019-03-17 08:59:42 +0100

    Justeringer i dialektspillet og adminsiden

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,6 +1,9 @@
 "use strict";
 const db = firebase.firestore();
 
+let map;
+let aktiveMarkorer = 0;
+let resetVal;
 function initMap() {
     const initLocation = { lat: 65, lng: 15 };
     map = new google.maps.Map(document.getElementById('map'), {
@@ -8,25 +11,54 @@ function initMap() {
         center: initLocation
     });
 }
-function oppdaterScoreBoard() {
-    // Tømmer scoreboarden
-    const dialektScoreEl = document.querySelector("#scoreDialekt");
-    dialektScoreEl.innerHTML = "";
-    let scoreliste = [];
+let scoreliste = [];
+let markers = [];
+function hentStats() {
+    clearOverlays();
+    markers = [];
+    scoreliste = [];
+
+    // Tømmer scoreboardene
+    document.querySelector("#scoreDialekt").innerHTML = "";
+    document.querySelector("#scoreQuiz").innerHTML = "";
+    clearOverlays();
+
+
 
     db.collection("HSDialektspillet").get().then((querySnapshot) => {
         querySnapshot.forEach((entry) => {
             const nyScore = {
                 spillernavn: entry.data().spillernavn,
                 score: entry.data().score,
-                ID: entry.data().ID
+                ID: entry.data().ID,
+                valgteSvar: entry.data().valgtePosisjoner
+            };
+            scoreliste.push(nyScore);
+        });
+        scoreliste.sort(compare);
+        resetVal = scoreliste[0].valgteSvar.length;
+        for (let i = 0; i < scoreliste.length; i++) {
+            leggTilPaaScoreBoard(scoreliste[i].spillernavn, scoreliste[i].score, "#scoreDialekt");
+            if (scoreliste[i].valgteSvar[aktiveMarkorer] !== undefined) {
+                addMarker(scoreliste[i].valgteSvar[aktiveMarkorer]);
+            }
+        }
+    });
 
+    db.collection("HSQuiz").get().then((querySnapshot) => {
+        let scoreliste = [];
+        querySnapshot.forEach((entry) => {
+            const nyScore = {
+                spillernavn: entry.data().spillernavn,
+                score: entry.data().score,
+                ID: entry.data().ID,
+                valgteSvar: valgtePosisjoner
             };
             scoreliste.push(nyScore);
         });
         scoreliste.sort(compare);
         for (let i = 0; i < scoreliste.length; i++) {
-            leggTilPaaScoreBoard(scoreliste[i].spillernavn, scoreliste[i].score);
+            leggTilPaaScoreBoard(scoreliste[i].spillernavn, scoreliste[i].score, "#scoreQuiz");
         }
     });
     function compare(a, b) {
@@ -36,9 +68,10 @@ function oppdaterScoreBoard() {
             return 1;
         return 0;
     }
-    function leggTilPaaScoreBoard(spillernavn, score) {
+    function leggTilPaaScoreBoard(spillernavn, score, containerSel) {
+        const container = document.querySelector(containerSel);
         const nyDiv = document.createElement("div");
-        dialektScoreEl.appendChild(nyDiv);
+        container.appendChild(nyDiv);
         const nyttNavnEl = document.createElement("h4");
         nyttNavnEl.innerHTML = spillernavn;
         const nyttScoreEl = document.createElement("p");
@@ -46,5 +79,26 @@ function oppdaterScoreBoard() {
         nyDiv.appendChild(nyttNavnEl);
         nyDiv.appendChild(nyttScoreEl);
     }
+
+    function addMarker(location) {
+        const marker = new google.maps.Marker({
+            position: location,
+            map: map
+        });
+        markers.push(marker);
+    }
+    function clearOverlays() {
+        for (var i = 0; i < markers.length; i++) {
+            markers[i].setMap(null);
+        }
+        markers.length = 0;
+    }
 }
-oppdaterScoreBoard();
+function endreAktiveMarkorer(endring) {
+    aktiveMarkorer = Math.abs((aktiveMarkorer + endring) % resetVal);
+    hentStats();
+    console.log(aktiveMarkorer);
+}
+document.querySelector("#knappTilbake").addEventListener("click", () => { endreAktiveMarkorer(-1); });
+document.querySelector("#knappFrem").addEventListener("click", () => { endreAktiveMarkorer(1); });
+hentStats();
