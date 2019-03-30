162f7cf8d459d9f306cf8c9fe43a7168d44ce8d9
emlie <mi-ya@live.no>
2019-03-30 16:55:52 +0100

    fiksa dialektsida

--- scripts/infoDialekt.js
+++ scripts/infoDialekt.js
@@ -53,70 +53,48 @@ let data = [
   },
   {
     type: 'vestlandsk',
-    fylker: ["Aust-Agder", "Vest-Agder", "Rogaland", "Sogn og Fjordane", "Møre og Romsdal"],
-    tjukkL: "Nei\n" + "Bortsett fra noen områder i Møre og Romsdal",
-    palatalisering: "Nei\n" + "Bortsett fra området nord for Sognefjorden",
-    infinitiv: "E-infinitiv i Agder og i Møre og Romsdal" + "A-infinitiv i Rogaland og Hordaland",
-    personligePronomen: "Mest \"eg/e/i\" i entall \n" +
-    "og \"me/mi\" i flertall",
-    svakeHunkjonnsord: "Mest -o/-å \n" +
-    "(eks:: \"klokkå\")\n" +
-    "Men en del -a i Agder og S og Fj\n",
-    sterkeHunkjonnsord: "Svært blandet.\n" +
-    "-a i sør og nord (\"eks.: \"boka\")\n" +
-    "Ellers -o/-å (kysten) \n" +
-    "eller -e/-i/-æ/-ei (innlandet)",
-    skarreR: "Ja.\n" +
-    "Minus området nord for Sognefjordane",
-    bloteKonsonanter: "Ja,\n" +
-    "på Agderkysten og i det meste av Rogaland. \n" +
-    "Ellers harde kons."
+    fylker: "Aust-Agder, Vest-Agder, Rogaland, Sogn og Fjordane, Møre og Romsdal",
+    tjukkL: "Nei, bortsett fra noen områder i Møre og Romsdal",
+    palatalisering: "Nei, bortsett fra området nord for Sognefjorden",
+    infinitiv: "E-infinitiv i Agder og i Møre og Romsdal og A-infinitiv i Rogaland og Hordaland",
+    personligePronomen: "Mest \"eg/e/i\" i entall og \"me/mi\" i flertall",
+    svakeHunkjonnsord: "Mest -o/-å (eks. \"klokkå\"), men en del -a i Agder og S og Fj",
+    sterkeHunkjonnsord: "Svært blandet. -a i sør og nord (eks. \"boka\"). Ellers -o/-å (kysten) eller -e/-i/-æ/-ei (innlandet)",
+    skarreR: "Ja, bortsett fra området nord for Sognefjordane",
+    bloteKonsonanter: "Ja, på Agderkysten og i det meste av Rogaland. Ellers harde kons."
   },
   {
     type: 'østlandsk',
-    fylker: ["Telemark", "Vestfold", "Østfold", "Akershus", "Buskerud", "Oppland", "Hedmark"],
-    tjukkL: "Ja\n" + "Bortsett fra Vest-Telemark",
-    palatalisering: "Nei\n" + "Bortsett fra Hedmark og nordlige Oppland",
-    infinitiv: "Kløyvd infinitiv\n" +
-    "a-ending i noen verb.\n" +
-    "e-ending i de andre.",
-    personligePronomen: "Lavlandet har mest \"jeg/je\" og \"vi/ve\"\n" +
-    "Fjellbygdene i vest følger Vestlandsk.\n" +
-    "Nordlige Oppland har flertall \"oss\".",
-    svakeHunkjonnsord: "Bare -a (eks.: \"klokka\")",
-    sterkeHunkjonnsord: "Mest -a.\n" +
-    "Men -e/-i/-æ/-ei i fjellbygdene i vest.",
+    fylker: "Telemark, Vestfold, Østfold, Akershus, Buskerud, Oppland, Hedmark",
+    tjukkL: "Ja, bortsett fra Vest-Telemark",
+    palatalisering: "Nei, bortsett fra Hedmark og nordlige Oppland",
+    infinitiv: "Kløyvd infinitiv. a-ending i noen verb. e-ending i de andre.",
+    personligePronomen: "Lavlandet har mest \"jeg/je\" og \"vi/ve\". Fjellbygdene i vest følger Vestlandsk. Nordlige Oppland har flertall \"oss\".",
+    svakeHunkjonnsord: "Bare -a (eks. \"klokka\")",
+    sterkeHunkjonnsord: "Mest -a, men -e/-i/-æ/-ei i fjellbygdene i vest.",
     skarreR: "Nei",
     bloteKonsonanter: "Nei"
   },
   {
     type: 'trøndsk',
-    fylker: ["Møre og Romsdal", "Sør-Trøndelag", "Nord-Trøndelag"],
+    fylker: "Møre og Romsdal, Sør-Trøndelag, Nord-Trøndelag",
     tjukkL: "Ja",
     palatalisering: "Ja",
-    infinitiv: "Kløyvd infinitiv\n" +
-    "a- eller å-ending i noen verb.\n" +
-    "Ingen ending i de andre (apokope).",
+    infinitiv: "Kløyvd infinitiv. a- eller å-ending i noen verb. Ingen ending i de andre (apokope).",
     personligePronomen: "Svært vekslende",
-    svakeHunkjonnsord: "Mest -a\n" +
-    "Men noe -o/-å på kysten sør for Trondh.fjorden",
+    svakeHunkjonnsord: "Mest -a, men noe -o/-å på kysten sør for Trondh.fjorden",
     sterkeHunkjonnsord: "Bare -a",
     skarreR: "Nei",
     bloteKonsonanter: "Nei"
   },
   {
     type: 'nordnorsk',
-    fylker: ["Nordland", "Troms", "Finnmark"],
-    tjukkL: "Nei\n" +
-    "Bortsett fra i det meste av Nordland og i Bardu og Målselv",
+    fylker: "Nordland, Troms, Finnmark",
+    tjukkL: "Nei, bortsett fra i det meste av Nordland og i Bardu og Målselv",
     palatalisering: "Ja",
-    infinitiv: "E-infinitiv.\n" +
-    "Men ending forsvinner i alle eller de fleste verb i Nordland.\n" +
-    "Kløyvd inf. i Bardu og Målselv.",
-    personligePronomen: "Mest \"æg/æ\" i entall\n" +
-    "og \"vi/ve\" i flertall",
-    svakeHunkjonnsord: "Mest -a\n" +
-    "Minus Nordland hvor det veksler svært.",
+    infinitiv: "E-infinitiv, men ending forsvinner i alle eller de fleste verb i Nordland. Kløyvd inf. i Bardu og Målselv.",
+    personligePronomen: "Mest \"æg/æ\" i entall og \"vi/ve\" i flertall",
+    svakeHunkjonnsord: "Mest -a, bortsett fra Nordland hvor det veksler svært.",
     sterkeHunkjonnsord: "Bare -a",
     skarreR: "Nei",
     bloteKonsonanter: "Nei"
@@ -231,7 +209,7 @@ function insertDialektSide() {
   // table side
   merker.forEach(merke => {
     dialektSide.innerHTML += `
-      <div class="centerV">
+      <div class="centerV side">
         <h5>${merke}</h5>
       </div>
     `;
@@ -317,22 +295,9 @@ function insertData() {
 
 
 
-/* ---------- GET SPECIFIC DATA ---------- */
-
-function getPropVal(obj, prop) {
-  return obj.prop;
-}
-
-
-
-
-
 /* ---------- INSERT DATA ---------- */
 
 function insertMainData() {
-
-  let object;
-  let property;
 
   let allProps = [];
   let allUniqueProps = [];
@@ -345,56 +310,125 @@ function insertMainData() {
     }
   });
 
+
   // Remove duplicates automatically with Set
   allUniqueProps = [...new Set(allProps)];
 
-  // let allColParts = Array.from(document.getElementsByClassName('colPart'));
-
-  // console.log(allDialektMainColsArr);
-  console.log(allUniqueProps);
-  // console.log(allColParts);
 
   for (let i = 1; i <= allDialektMainColsArr.length; i++) {
-
-    object = data[i];
-    console.log(object);
 
     for (let k = 1; k < allUniqueProps.length; k++) {
 
       property = allUniqueProps[k];
 
-      let info = getPropVal(object, property);
+      // this ain't quite right, but it works
+      function getPropVal(string) {
+        switch (string) {
+          case 'type':
+            allDialektMainColsArr[i].innerHTML += `
+              <div class="colPart">
+                <p>${data[i].type}</p>
+              </div>
+            `;
+            break;
+          case 'fylker':
+            allDialektMainColsArr[i].innerHTML += `
+              <div class="colPart">
+                <p>${data[i].fylker}</p>
+              </div>
+            `;
+            break;
+          case 'tjukkL':
+            allDialektMainColsArr[i].innerHTML += `
+              <div class="colPart">
+                <p>${data[i].tjukkL}</p>
+              </div>
+            `;
+            break;
+          case 'palatalisering':
+            allDialektMainColsArr[i].innerHTML += `
+              <div class="colPart">
+                <p>${data[i].palatalisering}</p>
+              </div>
+            `;
+            break;
+          case 'infinitiv':
+            allDialektMainColsArr[i].innerHTML += `
+              <div class="colPart">
+                <p>${data[i].infinitiv}</p>
+              </div>
+            `;
+            break;
+          case 'personligePronomen':
+            allDialektMainColsArr[i].innerHTML += `
+              <div class="colPart">
+                <p>${data[i].personligePronomen}</p>
+              </div>
+            `;
+            break;
+          case 'svakeHunkjonnsord':
+            allDialektMainColsArr[i].innerHTML += `
+              <div class="colPart">
+                <p>${data[i].svakeHunkjonnsord}</p>
+              </div>
+            `;
+            break;
+          case 'sterkeHunkjonnsord':
+            allDialektMainColsArr[i].innerHTML += `
+              <div class="colPart">
+                <p>${data[i].sterkeHunkjonnsord}</p>
+              </div>
+            `;
+            break;
+          case 'skarreR':
+            allDialektMainColsArr[i].innerHTML += `
+              <div class="colPart">
+                <p>${data[i].skarreR}</p>
+              </div>
+            `;
+            break;
+          case 'bloteKonsonanter':
+            allDialektMainColsArr[i].innerHTML += `
+              <div class="colPart">
+                <p>${data[i].bloteKonsonanter}</p>
+              </div>
+            `;
+            break;
+          default:
+            return true;
+        }
+      }
 
-      allDialektMainColsArr[i].innerHTML += `
-        <div class="colPart">
-          <p>${object}</p>
-        </div>
-      `;
+      getPropVal(property);
 
     }
   }
 
-  /*
-  allDialektMainColsArr.forEach(col => {
-    data.forEach(area => {
-      for (let property in area) {
-        col.innerHTML += `
-          <div class="text">
-            <p>${area[property]}</p>
-          </div>
-        `;
-      }
-    });
-  });
-  */
-
-  /*
-  for (let i = 1; i < array.length; i++) {
-    array[i];
-  }
-  */
 
   console.log('insertMainData');
+
+}
+
+
+
+
+
+/* ---------- ADJUST SIDE ---------- */
+
+function styleTableSide() {
+
+  let aColPart = document.getElementsByClassName('colPart')[0];
+  let acolPartCompHeight = window.getComputedStyle(aColPart).height;
+  let aColPartHeight = pxToNum(acolPartCompHeight);
+
+  let allSides = Array.from(document.getElementsByClassName('side'));
+
+
+  allSides.forEach(div => {
+    div.style.setProperty('--sideHeight', `${aColPartHeight}px`);
+  });
+
+  console.log(allSides);
 
 }
 
@@ -413,3 +447,5 @@ footerTop();
 // insertData();
 insertDialektSide();
 insertMainData();
+
+// styleTableSide();
