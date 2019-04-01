2676bac994754cd4db3477653e8ff5c7cc2d6dd5
emlie <mi-ya@live.no>
2019-04-02 00:06:16 +0200

    fix text for historie

--- scripts/infoHistorie.js
+++ scripts/infoHistorie.js
@@ -48,8 +48,6 @@ Interessen fikk sterk vekst ved de undersøkelser av norsk historie og norske fo
     year: 1840,
     text: `Det nasjonale gjennombrudd i 1840-årene førte til en språklig fornyelse på flere måter. Asbjørnsen og Moes gjenfortelling av folkeeventyrene gav grunnlaget for den nye norske prosa, som gjennomførte norsk stiltone i det tradisjonelle skriftspråket. Samtidig tok overlærer Knud Knudsen opp arbeidet for fornorsking på alle områder av språket (ordforrådet, orddannelse, bøyning, rettskrivning, uttale). Så kom fra 1848 de viktige arbeider av Ivar Aasen over det norske folkemål. På grunnlag av disse arbeidene, som viste sammenhengen mellom gammelnorsk og norske dialekter, bygde Aasen sin landsmålsnorm (første gang satt frem i Prøver af Landsmaalet i Norge, 1853). I sin normalform tok Aasen særlig hensyn til de dialekter som stod gammelnorsk nærmest så vel i bøyningsforhold som i lydforhold og ordforråd. Se også artikkel om norsk målreising. <br><br>
 Aasens lansering av et norsk skriftspråk, og Wergelands og Knudsens programmer for gradvis fornorsking av det danske, førte til to konkurrerende retninger. I 1885 ble landsmålet jamstilt som offisiell språkform ved at Stortinget vedtok kirkekomiteens henstilling til regjeringen: «Regjeringen anmodes om at træffe fornøden Forføining til, at det norske Folkesprog som Skole- og officielt Sprog sidestilles med vort almindelige Skrift- og Bogsprog» («jamstillingsvedtaket»). I folkeskolen ble jamstillingen fastslått ved folkeskoleloven av 1892, som overlot til skolestyret å bestemme om landsmål eller bokmål skulle brukes som opplærings- og lærebokmål i kommunen. Ved lærerskolene ble skriftlig prøve i begge mål innført ved lov i 1902, i gymnaset i 1907.<br><br>
-Fra 1900-tallet har utviklingen vært preget av de store rettskrivningsreformene: 1901 for landsmålet, 1907 for riksmålet, en mindre i 1910 for landsmål, og så for begge mål i 1917 og 1938, den siste med revisjon i 1959 og for bokmål i 1981 og 2005, foruten en del detaljrevisjoner etter forslag fra Norsk språkråd. For riksmål/bokmål har virkningen vært at det fra 1907 helt klart er et norsk skriftspråk og ikke et dansk. Der det er avvik mellom historisk skrivemåte og vanlig uttale, har begge mål nærmet seg uttalegjengivelse, og landsmål/nynorsk har gradvis tatt mer hensyn til østnorsk talemål og bydialektene.<br><br>
-Helt fra begynnelsen av 1900-tallet har det vært arbeidet på å fjerne unødige forskjeller mellom de to mål. Norsk språknemnd (1952–1972) skulle fremme tilnærming mellom de to skriftmål «på norsk folkemåls grunn»; Norsk språkråd (1972–2004) skulle «støtte opp om utviklingstendenser som på lengre sikt fører målformene nærmere sammen». Om målet skulle være ett «samnorsk» skriftspråk eller ikke, og hvor snarlig det eventuelt skulle gjennomføres, har vært det store språkstridsemne i mellom- og etterkrigstiden. Reformen i bokmålet i 1981 åpnet for en rekke tradisjonelle former som var blitt stengt ute tidligere, særlig i 1938, og etter det har hovedtendensen vært konsolidering. I 2002 ble Norsk språkråds tilnærmingsparagraf opphevet av Stortinget, og i 2005 ble en del lite brukte former, særlig tilnærmingsformer, tatt ut av normalen.<br><br>
 Mens det språklige reformarbeidet på 1800-tallet var sterkt preget av enkeltpersoners initiativ overfor myndigheter og publikum (I. Aasen, K. Knudsen, D.F. Knudsen, Moltke Moe med flere), har arbeidet på 1900-tallet vært drevet ved offentlig oppnevnte komiteer og private språkorganisasjoner, slik som Riksmålsforbundet (fra 1907), Noregs Mållag (fra 1906) og Landslaget for språklig samling (fra 1959). De store avgjørelsene tas nå som før av Stortinget. Motstand mot den offisielle språkpolitikken og særlig mot Norsk språknemnds mandat og arbeid førte til at språkkonservative organisasjoner på begge sider i perioder har holdt seg borte fra den offisielle språknormering og drevet privat normering på tvers av denne. For å skape «språkfred» nedsatte kirke- og undervisningsminister Helge Sivertsen Vogt-komiteen i 1964. Blant forslag fra komiteen som ble gjennomført, var opprettelsen av Norsk språkråd, som fikk bred representasjon også fra språkorganisasjonene. I 2005 ble Norsk språkråd erstattet av et mindre organ uten rådsforsamling, men med et departementsoppnevnt styre og et sekretariat, kalt Språkrådet.<br><br>
 I 1929 endret Stortinget de offisielle navn på språkformene fra landsmål til nynorsk og fra riksmål til bokmål.<br><br>
 Språkutviklingen i vår tid preges sterkt av tilstrømningen av lånord, særlig fra engelsk (anglisismer) og svensk (svesismer). Flytting, pendling, massemedia og andre sider ved samfunnsutviklingen bidrar til å jevne ut ulikheter i talemålet. Men store og rasktvirkende endringer i skole, arbeidsliv og så videre har skapt større språklig ulikhet mellom eldre og yngre, yrkesaktive og ikke-yrkesaktive. Språket i massemedia og administrasjon øver sterkere innflytelse på språksamfunnet som helhet i vår tid enn frem til om lag 1900. Tendensen til at komplisert fagspråk «invaderer» allmennspråket, særlig i trykte, eterbaserte og digitale media, blir balansert av enklere strukturer, for eksempel i form av kortere setninger og en mindre komplisert setningsbygning.<br><br>
@@ -68,7 +66,8 @@ Språkutviklingen i vår tid preges sterkt av tilstrømningen av lånord, særli
     year: 1907,
     text: `rettskrivingsreform – riksmål (bløte konsonanter fjernes) <br>` +
         `<ul><li>sidemålsstilen – alle skal beherske begge målformer </li>
-        <li>Norsk Riksmålsforbund stiftes – kamporganisasjon. </li></ul> <p><br>Første rettskrivningsreform, dansk-norsken får navnet "riksmål"</p>`
+        <li>Norsk Riksmålsforbund stiftes – kamporganisasjon. </li></ul> <p><br>Første rettskrivningsreform, dansk-norsken får navnet "riksmål"<br><br>
+        Fra 1900-tallet har utviklingen vært preget av de store rettskrivningsreformene: 1901 for landsmålet, 1907 for riksmålet, en mindre i 1910 for landsmål, og så for begge mål i 1917 og 1938, den siste med revisjon i 1959 og for bokmål i 1981 og 2005, foruten en del detaljrevisjoner etter forslag fra Norsk språkråd. For riksmål/bokmål har virkningen vært at det fra 1907 helt klart er et norsk skriftspråk og ikke et dansk. Der det er avvik mellom historisk skrivemåte og vanlig uttale, har begge mål nærmet seg uttalegjengivelse, og landsmål/nynorsk har gradvis tatt mer hensyn til østnorsk talemål og bydialektene.</p>`
   },
   {
     year: 1917,
@@ -78,8 +77,14 @@ Språkutviklingen i vår tid preges sterkt av tilstrømningen av lånord, særli
  <li>begge kan bruke hokjønnsbøying eller felleskjønn (hankjønn) → valgfrihet</li>
  <li>før: større forskjell mellom målene</li>
  <li>dansken mykes opp – mer fornorsking</li>
-</ul></ul>
+</ul></ul><p>
+<br>Helt fra begynnelsen av 1900-tallet har det vært arbeidet på å fjerne unødige forskjeller mellom de to mål. Norsk språknemnd (1952–1972) skulle fremme tilnærming mellom de to skriftmål «på norsk folkemåls grunn»; Norsk språkråd (1972–2004) skulle «støtte opp om utviklingstendenser som på lengre sikt fører målformene nærmere sammen». Om målet skulle være ett «samnorsk» skriftspråk eller ikke, og hvor snarlig det eventuelt skulle gjennomføres, har vært det store språkstridsemne i mellom- og etterkrigstiden. Reformen i bokmålet i 1981 åpnet for en rekke tradisjonelle former som var blitt stengt ute tidligere, særlig i 1938, og etter det har hovedtendensen vært konsolidering. I 2002 ble Norsk språkråds tilnærmingsparagraf opphevet av Stortinget, og i 2005 ble en del lite brukte former, særlig tilnærmingsformer, tatt ut av normalen.<br><br>
+</p>
 `
+  },
+  {
+    year: 1929,
+    text: "Stortinget endrer \"landsmål og riksmål\" til \"nynorsk og bokmål\"."
   },
   {
     year: 1938,
@@ -101,7 +106,10 @@ Språkutviklingen i vår tid preges sterkt av tilstrømningen av lånord, særli
   },
   {
     year: 1972,
-    text: `Norsk språkråd opprettet. Samnorsktanken forlates og språkstriden stilner
+    text: `Norsk språkråd opprettet. Samnorsktanken forlates og språkstriden stilner.
+    <br><br>
+    Mens det språklige reformarbeidet på 1800-tallet var sterkt preget av enkeltpersoners initiativ overfor myndigheter og publikum (I. Aasen, K. Knudsen, D.F. Knudsen, Moltke Moe med flere), har arbeidet på 1900-tallet vært drevet ved offentlig oppnevnte komiteer og private språkorganisasjoner, slik som Riksmålsforbundet (fra 1907), Noregs Mållag (fra 1906) og Landslaget for språklig samling (fra 1959). De store avgjørelsene tas nå som før av Stortinget. Motstand mot den offisielle språkpolitikken og særlig mot Norsk språknemnds mandat og arbeid førte til at språkkonservative organisasjoner på begge sider i perioder har holdt seg borte fra den offisielle språknormering og drevet privat normering på tvers av denne. For å skape «språkfred» nedsatte kirke- og undervisningsminister Helge Sivertsen Vogt-komiteen i 1964. Blant forslag fra komiteen som ble gjennomført, var opprettelsen av Norsk språkråd, som fikk bred representasjon også fra språkorganisasjonene. I 2005 ble Norsk språkråd erstattet av et mindre organ uten rådsforsamling, men med et departementsoppnevnt styre og et sekretariat, kalt Språkrådet.
+
 `
   },
   {
@@ -113,6 +121,10 @@ Språkutviklingen i vår tid preges sterkt av tilstrømningen av lånord, særli
     <p><br>  Ny nynorskreform der en del konservative former går ut</p>
 `
   },
+  {
+    year: "Nå",
+    text: "Språkutviklingen i vår tid preges sterkt av tilstrømningen av lånord, særlig fra engelsk (anglisismer) og svensk (svesismer). Flytting, pendling, massemedia og andre sider ved samfunnsutviklingen bidrar til å jevne ut ulikheter i talemålet. Men store og rasktvirkende endringer i skole, arbeidsliv og så videre har skapt større språklig ulikhet mellom eldre og yngre, yrkesaktive og ikke-yrkesaktive. Språket i massemedia og administrasjon øver sterkere innflytelse på språksamfunnet som helhet i vår tid enn frem til om lag 1900. Tendensen til at komplisert fagspråk «invaderer» allmennspråket, særlig i trykte, eterbaserte og digitale media, blir balansert av enklere strukturer, for eksempel i form av kortere setninger og en mindre komplisert setningsbygning"
+  }
 ];
 
 
@@ -134,7 +146,7 @@ function pxToNum(string) {
 
 function mainTop() {
   // adjust margin top
-  main.style.setProperty('--mainTop', `-${20*navHeight}px`);
+  main.style.setProperty('--mainTop', `-${24*navHeight}px`);
 }
 
 
